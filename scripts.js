157e7ac8a2fa87f9b9b8eec2a012e551a52b8968
Valdimar Örn <vos9@hi.is>
2020-10-25 12:45:36 +0000

    Kláraði verkefið

--- scripts.js
+++ scripts.js
@@ -3,26 +3,58 @@
  */
 
 const LETTERS = `AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ`;
-
+let code = true;
+"use strict";
 /**
  * Byrja forrit.
  */
 function start() {
-  input = prompt('Hvort viltu kóða eða afkóða streng?');
-  if ( input ==='kóða' ) {
-    let tala = prompt('Hversu mikið viltu hliðra streng?');
-    let n = parseInt(tala);
-    alert("n");
-  }
-  else if (input === 'afkóða' ){
-    let tala = prompt('Gefðu upp strenginn sem á að hliðra um' );
-  }
-  else {
-    alert('Veit ekki hvaða aðgerð',,'er. Reyndu aftur.');
-    start();
-  }
+  'use strict';
+  do{
+    var input = spurning();
+      if (input === 'kóða') {
+        let par = dulspurning(input);
+        alert(`Niðurstaða:\r\n ${encode(par.strengur,par.n)}`);
+      } else if (input === 'afkóða') {
+          let par = dulspurning(input);
+        alert(`Niðurstaða:\r\n ${decode(par.strengur,par.n)}`);
+      } else {
+          alert(`Veit ekki hvaða aðgerð ${input} er. Reyndu aftur.`);
+          start();
+      }
+  } while (confirm('Viltu halda áfram?'))
+}
+ 
+function spurning(){
+  let svar = prompt('Hvort viltu kóða eða afkóða streng');
+  return `${svar}`
 }
 
+function dulspurning(input){
+  let n = prompt('Hversu mikið á að hliðra streng? Gefðu upp heiltölu á bilinu [1, 31]');
+  if (n > -1 && n < 33){
+    let strengur = prompt(`Hvaða streng viltu ${input}?`);
+    var invalid = [];
+    for (var i = 0 ;i < strengur.length-1; i++) {
+      console.log(LETTERS.indexOf(strengur[i]));
+      if (LETTERS.indexOf(strengur[i]) === -1) {
+        invalid.push(strengur[i]);
+      }
+    }
+    if (strengur !== null || invalid.length === 0 ){
+      return {
+        strengur,
+        n,
+      }
+    } else {
+        alert(`Þú gafst upp stafi sem ekki er hægt að ${input}: ${invalid.join(', ')}. Reyndu aftur.`);
+        start();
+    }
+  } else {
+    alert(`${n} er ekki heiltala á bilinu [1, 31]. Reyndu aftur.`);
+    start();
+  } 
+}
 // Hér er gott að commenta út til að vinna í encode/decode föllum fyrst og síðan „viðmóti“ forrits
 start();
 
@@ -34,7 +66,13 @@ start();
  * @returns {string} Upprunalegi strengurinn hliðraður um n til hægri
  */
 function encode(str, n) {
-  return str;
+  let Caesar = LETTERS.slice(n, LETTERS.length)+LETTERS.slice(0,n-1);
+  let code = '';
+  for (var i = 0; i < str.length; i++) {
+    code += Caesar[LETTERS.indexOf(str[i])];
+    console.log(code);
+  }
+  return code;
 }
 
 /**
@@ -45,7 +83,14 @@ function encode(str, n) {
  * @returns {string} Upprunalegi strengurinn hliðraður um n til vinstri
  */
 function decode(str, n) {
-  return str;
+  let Caesar = LETTERS.slice(n, LETTERS.length)+LETTERS.slice(0,n-1);
+  let code = '';
+  console.log(Caesar)
+  for (var i = 0; i < str.length; i++) {
+    code += LETTERS[Caesar.indexOf(str[i])];
+    console.log(code);
+  }
+  return code;
 }
 
 console.assert(encode('A', 3) === 'D', 'kóðun á A með n=3 er D');
